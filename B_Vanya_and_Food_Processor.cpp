f87ac7e33e9bd9b15fedec9c293eb7ca03572732
ananthram <ananthramtennis12@gmail.com>
2022-01-07 23:10:20 +0530

    last commit of jan 7th

---
+++ B_Vanya_and_Food_Processor.cpp
@@ -0,0 +1,117 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define int long long
+#define pb push_back
+#define sz size
+#define all(c) c.begin(), c.end()
+#define endl '\n'
+int sum()
+{
+    return 0;
+}
+template <typename... Args>
+int sum(int a, Args... args) { return a + sum(args...); }
+
+string bin(long n)
+{
+    long i;
+    string val = "0";
+    for (i = 1 << 30; i > 0; i = i / 2)
+    {
+        if ((n & i) != 0)
+        {
+            val += "1";
+        }
+        else
+        {
+            val += "0";
+        }
+    }
+    while (val.front() == '0')
+    {
+        val.erase(0, 1);
+    }
+    return val;
+}
+
+void print() { cout << endl; };
+template <typename T, typename... Args>
+void print(T one, Args... rest)
+{
+    cout << one << " ";
+    print(rest...);
+}
+
+void read(){};
+template <typename T, typename... Args>
+void read(T &one, Args &...rest)
+{
+    cin >> one;
+    read(rest...);
+}
+
+template <typename T>
+void output_vec(vector<T> &v)
+{
+    for (auto x : v)
+    {
+        cout << x << " ";
+    }
+    cout << endl;
+}
+template <typename one, typename two>
+void output_map(map<one, two> &mp)
+{
+    for (auto itr = mp.begin(); itr != mp.end(); ++itr)
+    {
+        cout << itr->first << " " << itr->second << endl;
+    }
+}
+
+template <typename T>
+void output_set(set<T> &s)
+{
+    for (auto x : s)
+    {
+        cout << x << " ";
+    }
+    cout << endl;
+}
+void solve()
+{
+    int n, m, k;
+    read(n, m, k);
+    vector<int>arr(n);
+    for(int i=0;i<n;++i) cin >> arr[i];
+    int ans = 0,containing=0,i=0;
+    while(i<n){
+        while(containing+arr[i] <= m){
+            if (i >= n)
+                break;
+
+            containing+=arr[i];
+            i++;
+        }
+        if(containing<k){
+            containing= 0;ans++;
+        }else{
+            ans += (containing / k);
+            containing -= (k * (containing / k));
+        }
+        
+        // print(i,containing);
+    }
+    ans+=(containing/k)+(containing%k!=0);
+    cout << ans << endl;
+}
+
+int32_t main()
+{
+
+    cin.tie(NULL);
+    cout.tie(NULL);
+    ios_base::sync_with_stdio(false);
+
+    solve();
+}
