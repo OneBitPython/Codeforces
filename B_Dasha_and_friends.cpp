576c2edf4f7499d100204d7790455928d765b93d
ananthram <ananthramtennis12@gmail.com>
2022-01-31 19:17:07 +0530

    commited for Jan 31st

---
+++ B_Dasha_and_friends.cpp
@@ -0,0 +1,129 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define int long long
+#define pb push_back
+#define all(c) c.begin(), c.end()
+#define endl "\n"
+
+int sum() { return 0; }
+template<typename... Args>
+int sum(int a, Args... args) { return a + sum(args...); }
+
+string bin(long n){
+    long i;
+    string val = "0";
+    for (i = 1 << 30; i > 0; i = i / 2)
+    {
+      if((n & i) != 0)
+      {
+        val+="1";
+      }
+      else
+      {
+        val+="0";
+      }
+    }
+    while(val.front()=='0'){
+        val.erase(0,1);
+    }
+    return val;
+}
+
+void print(){cout << endl;};
+template <typename T, typename... Args>
+void print(T one, Args... rest){
+    cout << one << " ";
+    print(rest...);
+}
+
+void output(map<int, pair<int,int>>&mp){
+    for(auto itr=mp.begin();itr!=mp.end();++itr){
+        cout << itr->first << " - " << itr->second.first << " " << itr->second.second << endl;
+    }
+}
+
+void read(){};
+template<typename T, typename... Args>
+void read(T& one, Args&... rest){
+    cin >> one;
+    read(rest...);
+}
+
+template<typename T>
+void output(vector<T> &v){
+    for(auto x : v){
+        cout << x << " ";
+    }
+    cout << endl;
+}
+template<typename one,typename two>
+void output(map<one,two> &mp){
+    for(auto itr= mp.begin();itr!=mp.end();++itr){
+        cout << itr->first << " " << itr->second << endl;
+    }
+}
+
+template <typename T>
+void output(set<T>&s){
+    for(auto x : s){
+        cout << x << " ";
+    }
+    cout << endl;
+}
+template <typename T, typename W>
+void output(pair<T, W>&p){
+    cout << p.first << " " << p.second << endl;
+}
+template <typename T, typename W>
+void output(vector<pair<T, W>> &arr){
+    for(auto x : arr){
+        cout << x.first << " " << x.second << endl;
+    }
+}
+void solve()
+{
+    int n, l;
+    cin >> n >> l;
+    vector<int> a(n), b(n);
+    for(int i = 0;i<n;++i) cin >> a[i];
+    for(int i = 0;i<n;++i) cin >> b[i];
+    for(int i = 0;i<l;++i){
+        for(int j = 0;j<l;++j){
+            vector<int>pos1, pos2;
+            for(int k = 0;k<n;++k){
+                pos1.pb((i+a[k])%l);
+            }
+            for(int k = 0;k<n;++k){
+                pos2.pb((j+b[k])%l);
+            }
+            sort(all(pos1));
+            sort(all(pos2));
+            if(pos1 == pos2){
+                cout << "YES" << endl;
+                return;
+            }
+        }
+    }
+    cout << "NO" << endl;
+}
+
+int32_t main()
+{
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    
+
+    int T=1;
+    // read(T);
+    while (T--)
+    {
+        solve();
+    }
+}
