48657fac20fdd756d3caabfd9e9339e23c569fb8
ananthram <ananthramtennis12@gmail.com>
2022-04-11 22:35:42 +0530

    commited for april 11th

---
+++ B_PIN_Codes.cpp
@@ -0,0 +1,131 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define int long long
+#define pb push_back
+#define all(c) c.begin(), c.end()
+#define endl "\n"
+
+void __print(int x) {cerr << x;}
+void __print(long x) {cerr << x;}
+void __print(unsigned x) {cerr << x;}
+void __print(unsigned long x) {cerr << x;}
+void __print(unsigned long long x) {cerr << x;}
+void __print(float x) {cerr << x;}
+void __print(double x) {cerr << x;}
+void __print(long double x) {cerr << x;}
+void __print(char x) {cerr << '\'' << x << '\'';}
+void __print(const char *x) {cerr << '\"' << x << '\"';}
+void __print(const string &x) {cerr << '\"' << x << '\"';}
+void __print(bool x) {cerr << (x ? "true" : "false");}
+
+template<typename T, typename V>
+void __print(const pair<T, V> &x) {cerr << '{'; __print(x.first); cerr << ','; __print(x.second); cerr << '}';}
+template<typename T>
+void __print(const T &x) {int f = 0; cerr << '{'; for (auto &i: x) cerr << (f++ ? "," : ""), __print(i); cerr << "}";}
+void _print() {cerr << "]\n";}
+template <typename T, typename... V>
+void _print(T t, V... v) {__print(t); if (sizeof...(v)) cerr << ", "; _print(v...);}
+#ifndef ONLINE_JUDGE
+#define dbg(x...) cerr << "LINE(" << __LINE__ << ") -> " <<"[" << #x << "] = ["; _print(x)
+#else
+#define dbg(x...)
+#endif
+
+void print(){cerr << endl;};
+template <typename T, typename... Args>
+void print(T one, Args... rest){
+    cerr << one << " ";
+    print(rest...);
+}
+
+
+int sum() { return 0; }
+template<typename... Args>
+int sum(int a, Args... args) { return a + sum(args...); }
+
+string bin(long n){
+    long i;
+    string val = "0";
+    for (i = 1 << 30; i > 0; i = i / 2)
+    {
+      if((n & i) != 0)
+      {
+        val+="1";
+      }
+      else
+      {
+        val+="0";
+      }
+    }
+    while(val.front()=='0'){
+        val.erase(0,1);
+    }
+    return val;
+}
+
+
+
+
+void solve()
+{
+    int n;
+    cin >> n;
+    vector<string>a(n);
+    map<string,int>cnt;
+    for(int i = 0;i<n;++i){
+        cin >> a[i];
+        cnt[a[i]]++;
+    }
+    vector<string>res(n);
+    int changes = 0;
+    for(int i = 0;i<n;++i){
+        string val = a[i];
+        if(cnt[val] == 1){
+            res[i] = val;
+            continue;
+        }
+        bool ok = 0;
+        for(int y = 0;y<4;++y){
+            if(ok)break;
+            for(int j = 0;j<=9;++j){
+                char x = j+'0';
+                string v;
+                for(int k = 0;k<y;++k)v+=val[k];
+                v+=x;
+                for(int k = y+1;k<4;++k)v+=val[k];
+                if(cnt[v] == 0){
+                    cnt[v] = 1;
+                    res[i] = v;
+                    cnt[val]--;
+                    changes++;
+                    ok = 1;
+                    break;
+                }
+            }
+        }
+    }
+    cout << changes << endl;
+    
+    for(auto x : res)cout << x << endl;
+}   
+
+int32_t main()
+{
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    
+
+    int T=1;
+    cin >> T;
+    while (T--)
+    {
+        solve();
+    }
+}
