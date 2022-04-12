7a715fa07e839de7c5067135c3c2f359a67e4658
ananthram <ananthramtennis12@gmail.com>
2022-04-12 10:53:44 +0530

    commited for april 12th

---
+++ B_Minimize_the_Permutation.cpp
@@ -0,0 +1,124 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define int long long
+#define pb push_back
+#define all(c) c.begin(), c.end()
+#define endl "\n"
+
+void __print(int x) {cerr << x;}
+void __print(long x) {cerr << x;}
+void __print(unsigned x) {cerr << x;}
+void __print(unsigned long x) {cerr << x;}
+void __print(unsigned long long x) {cerr << x;}
+void __print(float x) {cerr << x;}
+void __print(double x) {cerr << x;}
+void __print(long double x) {cerr << x;}
+void __print(char x) {cerr << '\'' << x << '\'';}
+void __print(const char *x) {cerr << '\"' << x << '\"';}
+void __print(const string &x) {cerr << '\"' << x << '\"';}
+void __print(bool x) {cerr << (x ? "true" : "false");}
+
+template<typename T, typename V>
+void __print(const pair<T, V> &x) {cerr << '{'; __print(x.first); cerr << ','; __print(x.second); cerr << '}';}
+template<typename T>
+void __print(const T &x) {int f = 0; cerr << '{'; for (auto &i: x) cerr << (f++ ? "," : ""), __print(i); cerr << "}";}
+void _print() {cerr << "]\n";}
+template <typename T, typename... V>
+void _print(T t, V... v) {__print(t); if (sizeof...(v)) cerr << ", "; _print(v...);}
+#ifndef ONLINE_JUDGE
+#define dbg(x...) cerr << "LINE(" << __LINE__ << ") -> " <<"[" << #x << "] = ["; _print(x)
+#else
+#define dbg(x...)
+#endif
+
+void print(){cerr << endl;};
+template <typename T, typename... Args>
+void print(T one, Args... rest){
+    cerr << one << " ";
+    print(rest...);
+}
+
+
+int sum() { return 0; }
+template<typename... Args>
+int sum(int a, Args... args) { return a + sum(args...); }
+
+string bin(long n){
+    long i;
+    string val = "0";
+    for (i = 1 << 30; i > 0; i = i / 2)
+    {
+      if((n & i) != 0)
+      {
+        val+="1";
+      }
+      else
+      {
+        val+="0";
+      }
+    }
+    while(val.front()=='0'){
+        val.erase(0,1);
+    }
+    return val;
+}
+
+
+
+
+void solve()
+{
+    int n;
+    cin >> n;
+    vector<int>a(n);
+    for(int &u : a)cin >> u;
+    int op = 0;
+    int find = 0;
+    int taken_till = 0;
+    int k = 0;
+    while(find < n){
+
+        int pos = find;
+        // find min value in the range [find+1, n] which is lesser than a[find] and can be swapped to a[find] in the required operations
+        int tmp_op = op;
+        int min_ = a[find];
+        if(tmp_op>=n-1)break;
+        for(int i = find+1;i<n;++i){
+            if(tmp_op >= n-1)break;
+            if(a[i] < min_){
+                min_ = a[i];
+                pos = i;
+            }
+            tmp_op++;
+        }
+        for(int j = pos;j>=find+1;--j){
+            swap(a[j],a[j-1]);
+            op++;
+        }
+
+        find = max(find+1, pos);
+    }
+    for(auto x: a)cout << x << ' ' ;
+    cout << endl;
+}   
+
+int32_t main()
+{
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    
+
+    int T=1;
+    cin >> T;
+    while (T--)
+    {
+        solve();
+    }
+}
