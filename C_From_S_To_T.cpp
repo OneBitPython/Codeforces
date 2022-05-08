57d91746652700806fbc080b4c260558af8e18e0
ananthram <ananthramtennis12@gmail.com>
2022-05-08 14:52:58 +0530

    commited for may 8th

---
+++ C_From_S_To_T.cpp
@@ -0,0 +1,134 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define int long long
+#define pb push_back
+#define all(c) c.begin(), c.end()
+#define endl "\n"
+
+const double PI=3.14159265358979323846264338327950288419716939937510582097494459230;
+
+
+void __print(int x) {cerr << x;}
+void __print(long x) {cerr << x;}
+void __print(unsigned x) {cerr << x;}
+void __print(unsigned long x) {cerr << x;}
+void __print(unsigned long long x) {cerr << x;}
+void __print(float x) {cerr << x;}
+void __print(double x) {cerr << x;}
+void __print(long double x) {cerr << x;}
+void __print(char x) {cerr << '\'' << x << '\'';}
+void __print(const char *x) {cerr << '\"' << x << '\"';}
+void __print(const string &x) {cerr << '\"' << x << '\"';}
+void __print(bool x) {cerr << (x ? "true" : "false");}
+
+template<typename T, typename V>
+void __print(const pair<T, V> &x) {cerr << '{'; __print(x.first); cerr << ','; __print(x.second); cerr << '}';}
+template<typename T>
+void __print(const T &x) {int f = 0; cerr << '{'; for (auto &i: x) cerr << (f++ ? "," : ""), __print(i); cerr << "}";}
+void _print() {cerr << "]\n";}
+template <typename T, typename... V>
+void _print(T t, V... v) {__print(t); if (sizeof...(v)) cerr << ", "; _print(v...);}
+#ifndef ONLINE_JUDGE
+#define dbg(x...) cerr << "LINE(" << __LINE__ << ") -> " <<"[" << #x << "] = ["; _print(x)
+#else
+#define dbg(x...)
+#endif
+
+void print(){cerr << endl;};
+template <typename T, typename... Args>
+void print(T one, Args... rest){
+    cerr << one << " ";
+    print(rest...);
+}
+
+
+int sum() { return 0; }
+template<typename... Args>
+int sum(int a, Args... args) { return a + sum(args...); }
+
+string bin(long n){
+    long i;
+    string val = "0";
+    for (i = 1 << 30; i > 0; i = i / 2)
+    {
+      if((n & i) != 0)
+      {
+        val+="1";
+      }
+      else
+      {
+        val+="0";
+      }
+    }
+    while(val.front()=='0'){
+        val.erase(0,1);
+    }
+    return val;
+}
+
+
+
+
+void solve()
+{
+    string s,t,p;
+    cin >> s >> t >> p;
+    map<char,int>cnt1,cnt2,cnt3;
+    for(auto x : s)cnt1[x]++;
+    for(auto x : t)cnt2[x]++;
+    for(auto x : p)cnt3[x]++;
+    bool ok = 0;
+    // s should be subsequence of t
+    int curr = 0;
+    for(int i = 0;i<t.size();++i){
+        
+        if(t[i] == s[curr]){
+            curr++;
+        }
+        if(curr == s.size()){
+            ok = 1;
+            break;
+        }
+    }
+    if(!ok){
+        cout << "NO" << endl;
+        return;
+    }
+    for(auto itr:cnt2){
+        char x = itr.first;
+        int c = itr.second;
+        if(c<cnt1[x]){
+            cout << "NO" << endl;
+            return;
+        }else if(c > cnt1[x]){
+            int c2 = cnt1[x]+cnt3[x];
+            if(c2<c){
+                cout << "NO" << endl;
+                return;
+            }
+        }
+    }
+    
+    cout << "YES" << endl;
+}   
+
+int32_t main()
+{
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    
+
+    int T=1;
+    cin >> T;
+    for(int i = 1;i<=T;++i)
+    {
+        solve();
+    }
+}
