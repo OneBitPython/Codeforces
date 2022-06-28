54e578bb6e99d2aa232c9c16ca208ab662ceedea
ananthram <ananthramtennis12@gmail.com>
2022-06-28 22:58:00 +0530

    commited for 28th june

---
+++ Round803/D_Fixed_Point_Guessing.cpp
@@ -0,0 +1,115 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define int long long
+#define pb push_back
+#define all(c) c.begin(), c.end()
+
+const double PI=3.14159265358979323846264338327950288419716939937510582097494459230;
+
+
+void __print(int x) {cerr << x;}
+void __print(long x) {cerr << x;}
+void __print(unsigned x) {cerr << x;}
+void __print(unsigned long x) {cerr << x;}
+void __print(unsigned long long x) {cerr << x;}
+void __print(float x) {cerr << x;}
+void __print(double x) {cerr << x;}
+void __print(long double x) {cerr << x;}
+void __print(char x) {cerr << '\'' << x << '\'';}
+void __print(const char *x) {cerr << '\"' << x << '\"';}
+void __print(const string &x) {cerr << '\"' << x << '\"';}
+void __print(bool x) {cerr << (x ? "true" : "false");}
+
+template<typename T, typename V>
+void __print(const pair<T, V> &x) {cerr << '{'; __print(x.first); cerr << ','; __print(x.second); cerr << '}';}
+template<typename T>
+void __print(const T &x) {int f = 0; cerr << '{'; for (auto &i: x) cerr << (f++ ? "," : ""), __print(i); cerr << "}";}
+void _print() {cerr << "]\n";}
+template <typename T, typename... V>
+void _print(T t, V... v) {__print(t); if (sizeof...(v)) cerr << ", "; _print(v...);}
+#ifndef ONLINE_JUDGE
+#define dbg(x...) cerr << "LINE(" << __LINE__ << ") -> " <<"[" << #x << "] = ["; _print(x)
+#else
+#define dbg(x...)
+#endif
+
+void print(){cerr << endl;};
+template <typename T, typename... Args>
+void print(T one, Args... rest){
+    cerr << one << " ";
+    print(rest...);
+}
+
+
+int sum() { return 0; }
+template<typename... Args>
+int sum(int a, Args... args) { return a + sum(args...); }
+
+string bin(long n){
+    long i;
+    string val = "0";
+    for (i = 1 << 30; i > 0; i = i / 2)
+    {
+      if((n & i) != 0)
+      {
+        val+="1";
+      }
+      else
+      {
+        val+="0";
+      }
+    }
+    while(val.front()=='0'){
+        val.erase(0,1);
+    }
+    return val;
+}
+
+
+
+
+void solve()
+{
+    int n;
+    cin >> n;
+    int l = 1, r = n;
+    
+    while(l +1< r){
+        int m = (l+r)/2;
+        cout << "? " << l << ' ' << m << endl;
+        int cnt = 0;
+        for(int i = 0;i<(m-l+1);++i){
+            int v;
+            cin >> v;
+            if(v >= l && v <= m)cnt++;
+        }
+        
+        if(cnt%2)r = m;
+        else l = m;
+    }
+    cout << "? " << l << ' ' << l << endl;
+    int v;
+    cin >> v;
+    if(v == l)cout << "! " << v << endl;
+    else cout << "! " << r << endl;
+}   
+
+int32_t main()
+{
+
+    ios_base::sync_with_stdio(false);
+
+
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    
+
+    int T=1;
+    cin >> T;
+    for(int i = 1;i<=T;++i)
+    {
+        // cout << "Case #" << i << ": ";
+        solve();
+    }
+}
