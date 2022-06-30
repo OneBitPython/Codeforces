56b310814abcd0d7813af48790d1c2d234f28d92
ananthram <ananthramtennis12@gmail.com>
2022-06-30 22:35:23 +0530

    commited for june 30th

---
+++ E_2_Close_Tuples_hard_version.cpp
@@ -0,0 +1,157 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define int long long
+#define pb push_back
+#define all(c) c.begin(), c.end()
+#define endl "\n"
+
+const double PI=3.14159265358979323846264338327950288419716939937510582097494459230;
+
+
+void __print(int x) {cerr << x;}
+void __print(long x) {cerr << x;}
+void __print(unsigned x) {cerr << x;}
+void __print(unsigned long x) {cerr << x;}
+void __print(unsigned long long x) {cerr << x;}
+void __print(float x) {cerr << x;}
+void __print(double x) {cerr << x;}
+void __print(long double x) {cerr << x;}
+void __print(char x) {cerr << '\'' << x << '\'';}
+void __print(const char *x) {cerr << '\"' << x << '\"';}
+void __print(const string &x) {cerr << '\"' << x << '\"';}
+void __print(bool x) {cerr << (x ? "true" : "false");}
+
+template<typename T, typename V>
+void __print(const pair<T, V> &x) {cerr << '{'; __print(x.first); cerr << ','; __print(x.second); cerr << '}';}
+template<typename T>
+void __print(const T &x) {int f = 0; cerr << '{'; for (auto &i: x) cerr << (f++ ? "," : ""), __print(i); cerr << "}";}
+void _print() {cerr << "]\n";}
+template <typename T, typename... V>
+void _print(T t, V... v) {__print(t); if (sizeof...(v)) cerr << ", "; _print(v...);}
+#ifndef ONLINE_JUDGE
+#define dbg(x...) cerr << "LINE(" << __LINE__ << ") -> " <<"[" << #x << "] = ["; _print(x)
+#else
+#define dbg(x...)
+#endif
+
+void print(){cerr << endl;};
+template <typename T, typename... Args>
+void print(T one, Args... rest){
+    cerr << one << " ";
+    print(rest...);
+}
+
+
+int sum() { return 0; }
+template<typename... Args>
+int sum(int a, Args... args) { return a + sum(args...); }
+
+string bin(long n){
+    long i;
+    string val = "0";
+    for (i = 1 << 30; i > 0; i = i / 2)
+    {
+      if((n & i) != 0)
+      {
+        val+="1";
+      }
+      else
+      {
+        val+="0";
+      }
+    }
+    while(val.front()=='0'){
+        val.erase(0,1);
+    }
+    return val;
+}
+
+
+int N = 2e5+5;
+ll fact[(int)(2e5+5)];
+ll invFact[(int)(2e5+5)];
+int mod = 1e9+7;
+ll fast_pow(ll a, ll p)
+{
+    ll res = 1;
+    while (p)
+    {
+        if (p % 2 == 0)
+        {
+            a = (a * a) % mod;
+            p /= 2;
+        }
+        else
+        {
+            res = (res * a) % mod;
+            p--;
+        }
+    }
+    return res;
+}
+
+ll ncr(int n, int k)
+{
+    if (k > n)
+    {
+        return 0;
+    }
+    return fact[n] * invFact[k] % mod * invFact[n - k] % mod;
+}
+
+void solve()
+{
+    int n,m,k;
+    cin >> n>> m >> k;
+    vector<int>a(n);
+    for(int &u : a)cin >>u;
+    sort(all(a));
+    vector<pair<int,int>>ranges;
+    int res = 0;
+    int min_reached = n;
+    for(int i = n-1;i>=0;--i){
+        int idx = lower_bound(all(a), a[i]-k)-a.begin();
+        int cnt = i-idx+1;
+        if(cnt < m)continue;
+        if(idx < min_reached){
+            min_reached = idx;
+            ranges.pb({idx, i});
+            // nCr = n!/(n-r)!r! = cnt!/(cnt-m)!m!
+            res=((res%mod)+(ncr(cnt, m)%mod)%mod);
+        }
+    }
+    sort(all(ranges));
+    for(int i = 0;i<(int)(ranges.size()-1);++i){
+        int cnt = ranges[i].second - ranges[i+1].first + 1;
+        if(cnt<m)continue;
+        res = (((res%mod)-(ncr(cnt, m)%mod))+mod) % mod;
+    }
+    cout << res << endl;
+}   
+
+int32_t main()
+{
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+
+    fact[0] = invFact[0] = 1;
+    for (int i = 1; i < N; i++) {
+        fact[i] = (fact[i - 1] * i) % mod;
+        invFact[i] = fast_pow(fact[i], mod - 2);
+    }
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    
+    int T=1;
+    cin >> T;
+    for(int i = 1;i<=T;++i)
+    {
+        // cout << "Case #" << i << ": ";
+        solve();
+    }
+}
