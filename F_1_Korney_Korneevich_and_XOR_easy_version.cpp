8a15bf554896e1ce8d11f3ce9d8f8a657c8bf11e
OneBitPython <ananthramvijayaraj@gmail.com>
2023-02-12 00:44:16 +0530

    committed for feb12th

---
+++ F_1_Korney_Korneevich_and_XOR_easy_version.cpp
@@ -0,0 +1,86 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define int long long
+#define pb push_back
+#define all(c) c.begin(), c.end()
+#define endl "\n"
+
+const double PI=3.141592653589;
+
+
+void __print(int x) {cerr << x;}
+void __print(long x) {cerr << x;}
+void __print(unsigned x) {cerr << x;}
+void __print(unsigned long x) {cerr << x;}
+void __print(unsigned long long x) {cerr << x;}
+void __print(float x) {cerr << x;}
+void __print(double x) {cerr << x;}
+void __print(long double x) {cerr << x;}
+void __print(char x) {cerr << '\'' << x << '\'';}
+void __print(const char *x) {cerr << '\"' << x << '\"';}
+void __print(const string &x) {cerr << '\"' << x << '\"';}
+void __print(bool x) {cerr << (x ? "true" : "false");} 
+
+template<typename T, typename V>
+void __print(const pair<T, V> &x) {cerr << '{'; __print(x.first); cerr << ','; __print(x.second); cerr << '}';}
+template<typename T>
+void __print(const T &x) {int f = 0; cerr << '{'; for (auto &i: x) cerr << (f++ ? "," : ""), __print(i); cerr << "}";}
+void _print() {cerr << "]\n";}
+template <typename T, typename... V>
+void _print(T t, V... v) {__print(t); if (sizeof...(v)) cerr << ", "; _print(v...);}
+#ifndef ONLINE_JUDGE
+#define dbg(x...) cerr << "LINE(" << __LINE__ << ") -> " <<"[" << #x << "] = ["; _print(x)
+#else
+#define dbg(x...)
+#endif
+
+
+int N = 512;
+void solve()
+{
+    int n;
+    cin >> n;
+    vector<int>a(n+1);
+    for(int i = 1;i<=n;++i)cin >> a[i];
+    vector<vector<int>>dp(n+1, vector<int>(N+1,1e18));
+    for(int i = 1;i<=n;++i){
+        dp[i][a[i]] = min(dp[i][a[i]],a[i]);
+        for(int j = 0;j<=N;++j){
+            dp[i][j]=min(dp[i][j],dp[i-1][j]);
+            if(dp[i][j] < a[i]){
+                dp[i][j^a[i]] = min(dp[i][j^a[i]],a[i]);
+            }
+        }
+    }
+    vector<int>res;
+    res.pb(0);
+    for(int i = 1;i<=N;++i){
+        if(dp[n][i]<=N)res.pb(i);
+    }
+    cout << res.size() << endl;
+    for(auto x : res)cout << x << ' ';
+    cout << endl;
+}
+
+int32_t main()
+{
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    // #ifndef ONLINE_JUDGE
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    // freopen("error.txt", "w", stderr);
+    // #endif
+
+    int T=1;
+    // cin >> T;
+    for(int i = 1;i<=T;++i)
+    {
+        // cout << "Case #" << i << ": ";
+        solve();
+    }
+}
