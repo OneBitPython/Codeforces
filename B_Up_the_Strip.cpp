45e41a3e7005eac613fc93ba7976c1fae0ae519b
OneBitPython <ananthramvijayaraj@gmail.com>
2023-03-26 18:18:35 +0530

    committed for march 26th

---
+++ B_Up_the_Strip.cpp
@@ -0,0 +1,87 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define int long long
+#define pb push_back
+#define all(c) c.begin(), c.end()
+#define endl "\n"
+
+const double PI=3.141592653589;
+
+
+void __print(int x) {cerr << x;}
+void __print(long x) {cerr << x;}
+void __print(unsigned x) {cerr << x;}
+void __print(unsigned long x) {cerr << x;}
+void __print(unsigned long long x) {cerr << x;}
+void __print(float x) {cerr << x;}
+void __print(double x) {cerr << x;}
+void __print(long double x) {cerr << x;}
+void __print(char x) {cerr << '\'' << x << '\'';}
+void __print(const char *x) {cerr << '\"' << x << '\"';}
+void __print(const string &x) {cerr << '\"' << x << '\"';}
+void __print(bool x) {cerr << (x ? "true" : "false");} 
+
+template<typename T, typename V>
+void __print(const pair<T, V> &x) {cerr << '{'; __print(x.first); cerr << ','; __print(x.second); cerr << '}';}
+template<typename T>
+void __print(const T &x) {int f = 0; cerr << '{'; for (auto &i: x) cerr << (f++ ? "," : ""), __print(i); cerr << "}";}
+void _print() {cerr << "]\n";}
+template <typename T, typename... V>
+void _print(T t, V... v) {__print(t); if (sizeof...(v)) cerr << ", "; _print(v...);}
+#ifndef ONLINE_JUDGE
+#define dbg(x...) cerr << "LINE(" << __LINE__ << ") -> " <<"[" << #x << "] = ["; _print(x)
+#else
+#define dbg(x...)
+#endif
+
+int n,m;
+int ma(int a, int b){
+    return (a+b)%m;
+}
+int ms(int a, int b){
+    return ((((a%m)-(b%m))%m)+ m)%m;
+}
+
+void solve()
+{
+    cin >> n >> m;
+    vector<int>dp(n+1),suff(n+2);
+    dp[n] = 1;
+    suff[n] = 1;
+    for(int i = n-1;i>=1;--i){
+        dp[i] = ma(dp[i], suff[i+1]);
+        int add = 0;
+        for(int j = 2;;++j){    
+            if(i*j > n)break;
+            int till = min(n,(i*j)+j-1);
+            add = ma(add, ms(suff[i*j],suff[till+1]));
+        }
+        
+        dp[i] = ma(dp[i],add);
+        suff[i] = ma(suff[i+1],dp[i]);
+    }
+    cout << dp[1]%m << endl;
+}   
+
+int32_t main()
+{
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    // #ifndef ONLINE_JUDGE
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    // freopen("error.txt", "w", stderr);
+    // #endif
+
+    int T=1;
+    // cin >> T;
+    for(int i = 1;i<=T;++i)
+    {
+        // cout << "Case #" << i << ": ";
+        solve();
+    }
+}
